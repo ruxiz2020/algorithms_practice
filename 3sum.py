05a230a2c4c5f68d42c1e39aae4e5a464d569941
zruxi <zruxi@vmware.com>
2020-10-03 20:23:45 -0700

    readme update

---
+++ 3sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        N = len(nums)
+        nums.sort()
+        res = []
+        for t in range(N - 2):
+            if t > 0 and nums[t] == nums[t - 1]:
+                    continue
+            i, j = t + 1, N - 1
+            while i < j:
+                _sum = nums[t] + nums[i] + nums[j]
+                if _sum == 0:
+                    res.append([nums[t], nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+                elif _sum < 0:
+                    i += 1
+                else:
+                    j -= 1
+        return res
+
+ss = Solution()
+nums = [-1,0,1,2,-1,-4]
+res = ss.threeSum(nums)
+print('=== Given array : {}'.format(nums))
+print('=== result is : {}'.format(res))
