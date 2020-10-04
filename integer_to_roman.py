b2d48e79e6ab3835797dd3adb097d61225a892e3
zruxi <zruxi@vmware.com>
2020-10-03 19:29:04 -0700

    readme update

---
+++ integer_to_roman.py
@@ -0,0 +1,36 @@
+'''
+For example, 2 is written as II in Roman numeral, just two one's added together.
+12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However,
+the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+'''
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        list = ''
+        for i in range(0, len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                list += numerals[i]
+        return list
+
+
+ss = Solution()
+num = 1994
+res = ss.intToRoman(num)
+print('=== Given number : {}'.format(num))
+print('=== result is : {}'.format(res))
