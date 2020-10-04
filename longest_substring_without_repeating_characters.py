a2218771ce1fc3a3ff4d07a85f18714abbfd8c3f
zruxi <zruxi@vmware.com>
2020-10-03 17:58:46 -0700

    readme update

---
+++ longest_substring_without_repeating_characters.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left, right = 0, 0
+        chars = set()
+        res = 0
+        while left < len(s) and right < len(s):
+            if (s[right] in chars) and (s[left] in chars):
+                chars.remove(s[left])
+                left += 1
+            else:
+                chars.add(s[right])
+                right += 1
+                res = max(res, len(chars))
+        return res
+
+
+ss = Solution()
+s = "abcabcbb"
+res = ss.lengthOfLongestSubstring(s)
+print('=== Given string : {}'.format(s))
+print('=== result is : {}'.format(res))
