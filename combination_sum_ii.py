3521a02cc1293848deb548529d5a7d42c9bf48d6
zruxi <zruxi@vmware.com>
2020-10-04 12:22:53 -0700

    update readme

---
+++ combination_sum_ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        print(candidates)
+        res = []
+        self.dfs(candidates, target, 0, res, [])
+        return res
+
+    def dfs(self, nums, target, index, res, path):
+        if target < 0:
+            return
+        elif target == 0:
+            res.append(path)
+            return
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums, target - nums[i], i + 1, res, path + [nums[i]])
+
+ss = Solution()
+candidates = [2,5,2,1,2]
+target = 5
+print('=== Given candidates  : {}'.format(candidates))
+print('=== Given target  : {}'.format(target))
+res = ss.combinationSum2(candidates, target)
+
+print('=== result is : {}'.format(res))
